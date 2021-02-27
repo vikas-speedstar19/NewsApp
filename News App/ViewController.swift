765beed0c98f519a26a29c869116a9f8eeaadf65
Vikas Choure <vikas.choure@montyholding.com>
2021-02-27 14:49:47 +0530

    Initial Commit

---
+++ News App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News App
+//
+//  Created by monty on 27/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
